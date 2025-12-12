b4d0d26d48cd49b9068e0b2f7405eb9b63d425f0
hdg-zero <hdg-zero@pm.me>
2025-12-12 16:16:41 +0100

    build: setup astro

---
+++ site/astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://hdg-zero.github.io',
+    base: '/hdg-zero.github.io',
+});
