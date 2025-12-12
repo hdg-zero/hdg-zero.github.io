6a08da4a85f8e2b74ec0589d4a03bdaa0523ce30
hdg-zero <hdg-zero@pm.me>
2025-12-12 20:27:51 +0100

    fix: Astro base path configuration, and rephrase homepage headline.

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -6,7 +6,7 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://hdg-zero.github.io',
-  base: '/hdg-zero.github.io/',
+  base: '/',
 
   vite: {
     plugins: [tailwindcss()],
