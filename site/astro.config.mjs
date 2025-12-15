b06ea1576ee33f6d9b22f301c5ff9d63d9bb6d8f
hdg-zero <hdg-zero@pm.me>
2025-12-15 22:08:06 +0100

    build: add mdx

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://hdg-zero.github.io',
@@ -12,5 +14,5 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
 
-  integrations: [react()],
+  integrations: [react(), mdx()],
 });
